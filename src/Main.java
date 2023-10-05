11e47aac5fc67eca44844d4f0d9b826df7bd651b
LanCorC <lanc3.corcega@gmail.com>
2023-10-05 20:37:07 +1300

    Section 23.415(Concurrency in Java: Challenge 1 and 2): completing challenge1, create 2 threads and start them

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("12345-678", 1000.00);
+
+        //Own code here  - create and start threads
+        Thread t1 = new Thread(() -> {
+            System.out.println("ping");
+            account.deposit(300);
+            account.withdraw(50);
+        });
+        Thread t2 = new Thread(() -> {
+            System.out.println("pong");
+            account.deposit(203.75);
+            account.withdraw(100);
+        });
+
+        t1.start();
+        t2.start();
+
+//        new Thread(() -> {
+//            try {
+//                System.out.println("Starting sleep soon...");
+//                Thread.sleep(500);
+//                System.out.println("New balance: " + account.getBalance() + Thread.currentThread());
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }).start();
+    }
+
 }
