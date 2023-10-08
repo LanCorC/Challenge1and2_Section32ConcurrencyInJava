6768263987c94c392a85bd271f6ee2c2b4b2cdd4
Lance <lanc3.corcega@gmail.com>
2023-10-08 18:22:24 +1300

    Section 23.417(Concurrency in Java: Challenge 6 and 7): fixing up some code, "withdraw" had not been re-implemented in previous update

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,19 +1,63 @@
- public class BankAccount {
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BankAccount {
 
     private double balance;
     private String accountNumber;
+    private ReentrantLock lock;
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        lock = new ReentrantLock();
     }
 
-    public synchronized void deposit(double amount) {
-        balance += amount;
+    public void deposit(double amount) {
+
+        boolean status = false;
+
+        try {
+            if (lock.tryLock(1, TimeUnit.SECONDS)) {
+                try {
+                    balance += amount;
+                    status = true;
+//                    Thread.sleep(2000); //code to force the 'else' path
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock.");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Something went wrong: " + e);
+        }
+
+        System.out.println("Transaction status = " + status);
+
+
     }
 
     public synchronized void withdraw(double amount) {
-        balance -= amount;
+        boolean status = false;
+
+        try {
+            if (lock.tryLock(1, TimeUnit.SECONDS)) {
+                try {
+                    balance -= amount;
+                    status = true;
+//                    Thread.sleep(2000); //code to force the 'else' path
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock.");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Something went wrong: " + e);
+        }
+
+        System.out.println("Transaction status = " + status);
     }
 
     public double getBalance() {
